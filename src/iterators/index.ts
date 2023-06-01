138a1e5c23ba13bdf632ce47d86ab451043b5cfb
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2023-06-01 22:26:33 +0200

    :tada: Adds Results (#1)
    * :tada: Starts Adding Result
    
    * :arrow_up: Upgrades Deps

--- src/iterators/index.ts
+++ src/iterators/index.ts
@@ -1,6 +1,10 @@
-export { r, range } from './range';
-export { stepBy } from './stepBy';
-export { map } from './map';
-export { take } from './take';
+import { RustIterator } from './RustIterator';
+
 export { filter } from './filter';
+export { map } from './map';
+export { r, range } from './range';
 export { RustIterator } from './RustIterator';
+export { stepBy } from './stepBy';
+export { take } from './take';
+export { zip } from './zip';
+export default RustIterator;
