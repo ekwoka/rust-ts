f865be17c32e6ee5a1455c9147be797719752c99
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2023-06-12 22:26:35 +0400

    :sparkles: Adds Sorting (#4)

---
+++ src/iterators/sort.ts
@@ -0,0 +1,15 @@
+const lexographicalCompare = <T>(a: T, b: T) => (a > b ? 1 : a < b ? -1 : 0);
+
+export function* sort<T>(
+  iter: Iterable<T>,
+  compare: (a: T, b: T) => number = lexographicalCompare
+) {
+  let next = 0;
+  const collection: T[] = [...iter];
+  while (collection.length) {
+    for (let i = 1; i < collection.length; i++)
+      if (compare(collection[next], collection[i]) > 0) next = i;
+    yield collection.splice(next, 1)[0];
+    next = 0;
+  }
+}
