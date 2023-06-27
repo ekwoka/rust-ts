02dcfd2372350b8d428acae09ce29cb5b043f020
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2023-06-27 22:45:55 +0400

    :sparkles: More Result methods (#8)
    * :sparkles: Results orElse
    
    * :sparkles: Maps Results
    
    * :sparkles: Inspects values
    
    * :sparkles: Expects Ok values

--- src/result/Result.ts
+++ src/result/Result.ts
@@ -4,10 +4,22 @@ const err = Symbol();
 export interface Result<T, E> {
   isOk(): boolean;
   isErr(): boolean;
+
   unwrap(): T;
   unwrapOr(defaultValue: T): T;
   unwrapErr(): E;
+  expect(message: string): T;
+
   andThen<U>(op?: (value: T) => U): U | Err<E>;
+  orElse<U>(op?: (error: E) => U): U | Ok<T>;
+
+  map<U>(op: (value: T) => U): Result<U, E>;
+  mapErr<U>(op: (error: E) => U): Result<T, U>;
+  mapOr<U>(op: (value: T) => U, defaultValue: U): U;
+  mapOrElse<U>(op: (value: T) => U, opErr: (error: E) => U): U;
+
+  inspect(inspector: (value: T) => void): Result<T, E>;
+  inspectErr(inspector: (error: E) => void): Result<T, E>;
 }
 
 export class Ok<T> implements Result<T, never> {
@@ -28,8 +40,36 @@ export class Ok<T> implements Result<T, never> {
   unwrapErr(): never {
     throw new Error('called `Result.unwrapErr()` on an `Ok` value');
   }
+  expect(_msg: string): T {
+    return this.value;
+  }
+
   andThen<U>(op: (value: T) => U): U {
     return op(this.value);
+  }
+  orElse(): Ok<T> {
+    return this;
+  }
+
+  map<U>(op: (value: T) => U): Ok<U> {
+    return new Ok(op(this.value));
+  }
+  mapErr(): Ok<T> {
+    return this;
+  }
+  mapOr<U>(op: (value: T) => U, _defaultValue: U): U {
+    return op(this.value);
+  }
+  mapOrElse<U>(op: (value: T) => U, _opErr: (error: never) => U): U {
+    return op(this.value);
+  }
+
+  inspect(inspector: (value: T) => void): Ok<T> {
+    inspector(this.value);
+    return this;
+  }
+  inspectErr(): Ok<T> {
+    return this;
   }
 }
 
@@ -51,7 +91,37 @@ export class Err<E> implements Result<never, E> {
   unwrapErr(): E {
     return this.error;
   }
+  expect(msg: string): never {
+    throw new Error(
+      `Error unwrapping Err value: ${this.error}. Expected ${msg}`
+    );
+  }
+
   andThen(): Err<E> {
+    return this;
+  }
+  orElse<U>(op: (error: E) => U): U {
+    return op(this.error);
+  }
+
+  map(): Err<E> {
+    return this;
+  }
+  mapErr<U>(op: (error: E) => U): Err<U> {
+    return new Err(op(this.error));
+  }
+  mapOr<U>(_op: (value: never) => U, defaultValue: U): U {
+    return defaultValue;
+  }
+  mapOrElse<U>(_op: (value: never) => U, opErr: (error: E) => U): U {
+    return opErr(this.error);
+  }
+
+  inspect(_inspector: (value: never) => void): Err<E> {
+    return this;
+  }
+  inspectErr(inspector: (error: E) => void): Err<E> {
+    inspector(this.error);
     return this;
   }
 }
--- test/result/result.test.ts
+++ test/result/result.test.ts
@@ -25,33 +25,110 @@ describe('Result', () => {
     expect(isErr(err)).toEqual(true);
     expect(err.isErr()).toEqual(true);
   });
-  it('can unwrap an Ok result or throw Err', () => {
-    const okie = new Ok(42);
-    expect(okie.unwrap()).toEqual(42);
-    const err = new Err(new Error('42'));
-    expect(() => err.unwrap()).toThrow(new Error('42'));
+  describe('unwrap', () => {
+    it('can unwrap an Ok result or throw Err', () => {
+      const okie = new Ok(42);
+      expect(okie.unwrap()).toEqual(42);
+      const err = new Err(new Error('42'));
+      expect(() => err.unwrap()).toThrow(new Error('42'));
+    });
+    it('can unwrap an Err result', () => {
+      const okie = new Ok(42);
+      expect(() => okie.unwrapErr()).toThrow(
+        new Error('called `Result.unwrapErr()` on an `Ok` value')
+      );
+      const err = new Err(new Error('42'));
+      expect(err.unwrapErr()).toEqual(new Error('42'));
+    });
+    it('can unwrapOr use default', () => {
+      expect(new Ok(42).unwrapOr(69)).toEqual(42);
+      expect(new Err(42).unwrapOr(69)).toEqual(69);
+    });
+
+    it('can indicate why result is epected to be Ok', () => {
+      expect(new Ok(42).expect('value is hardcoded')).toEqual(42);
+      expect(() => new Err(42).expect('value is hardcoded')).toThrow(
+        /value is hardcoded/
+      );
+    });
   });
-  it('can unwrap an Err result', () => {
-    const okie = new Ok(42);
-    expect(() => okie.unwrapErr()).toThrow(
-      new Error('called `Result.unwrapErr()` on an `Ok` value')
-    );
-    const err = new Err(new Error('42'));
-    expect(err.unwrapErr()).toEqual(new Error('42'));
+  describe('control flow', () => {
+    it('can chain andThen', () => {
+      let result: Result<number, number> = new Ok(42);
+      result = result.andThen((value) => new Ok(value + 1));
+      expect(result.unwrap()).toEqual(43);
+      result = result.andThen((value) => new Err(value + 1));
+      expect(result.unwrapErr()).toEqual(44);
+      result = result.andThen((value) => new Ok(value + 1));
+      expect(result.unwrapErr()).toEqual(44);
+      result = result.andThen((value) => new Err(value + 1));
+      expect(result.unwrapErr()).toEqual(44);
+    });
+    it('can chain orElse', () => {
+      let result: Result<number, number> = new Err(42);
+      result = result.orElse((value) => new Ok(value + 1));
+      expect(result.unwrap()).toEqual(43);
+      result = result.orElse((value) => new Err(value + 1));
+      expect(result.unwrap()).toEqual(43);
+    });
   });
-  it('can unwrapOr use default', () => {
-    expect(new Ok(42).unwrapOr(69)).toEqual(42);
-    expect(new Err(42).unwrapOr(69)).toEqual(69);
+  describe('map', () => {
+    it('can map Ok', () => {
+      const okie: Result<number, number> = new Ok(42);
+      expect(okie.map((value) => value + 1).unwrap()).toEqual(43);
+      const error: Result<number, number> = new Err(42);
+      expect(error.map((value) => value + 1).unwrapErr()).toEqual(42);
+    });
+    it('can mapErr', () => {
+      const okie: Result<number, number> = new Ok(42);
+      expect(okie.mapErr((value) => value + 1).unwrap()).toEqual(42);
+      const error: Result<number, number> = new Err(42);
+      expect(error.mapErr((value) => value + 1).unwrapErr()).toEqual(43);
+    });
+    it('can mapOr', () => {
+      const okie = new Ok(42);
+      expect(okie.mapOr((value) => value + 1, 69)).toEqual(43);
+      const error = new Err(42);
+      expect(error.mapOr((value) => value + 1, 69)).toEqual(69);
+    });
+    it('can mapOrElse', () => {
+      const okie = new Ok(42);
+      expect(
+        okie.mapOrElse(
+          (value) => value + 1,
+          (value) => value - 1
+        )
+      ).toEqual(43);
+      const error = new Err(42);
+      expect(
+        error.mapOrElse(
+          (value) => value + 1,
+          (value) => value - 1
+        )
+      ).toEqual(41);
+    });
   });
-  it('can chain andThen', () => {
-    let result: Result<number, number> = new Ok(42);
-    result = result.andThen((value) => new Ok(value + 1));
-    expect(result.unwrap()).toEqual(43);
-    result = result.andThen((value) => new Err(value + 1));
-    expect(result.unwrapErr()).toEqual(44);
-    result = result.andThen((value) => new Ok(value + 1));
-    expect(result.unwrapErr()).toEqual(44);
-    result = result.andThen((value) => new Err(value + 1));
-    expect(result.unwrapErr()).toEqual(44);
+
+  describe('special methods', () => {
+    it('inspects the Ok value', () => {
+      const closure = vi.fn();
+      const okie = new Ok(42);
+      okie.inspect(closure);
+      expect(closure).toHaveBeenCalledOnce();
+      expect(closure).toHaveBeenCalledWith(42);
+      const err = new Err(42);
+      err.inspect(closure);
+      expect(closure).toHaveBeenCalledOnce();
+    });
+    it('inspects the Err value', () => {
+      const closure = vi.fn();
+      const okie: Result<number, number> = new Ok(42);
+      okie.inspectErr(closure);
+      expect(closure).toHaveBeenCalledTimes(0);
+      const err: Result<number, number> = new Err(42);
+      err.inspectErr(closure);
+      expect(closure).toHaveBeenCalledOnce();
+      expect(closure).toHaveBeenCalledWith(42);
+    });
   });
 });
