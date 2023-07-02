f6f402c45ae48d22e560e135657097283bc33c67
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2023-07-02 16:41:58 +0400

    :tada: TryJson

---
+++ src/upgrade/JSON.ts
@@ -0,0 +1,13 @@
+import { Result, Try } from '@/result/Result';
+
+if (!JSON.tryParse) {
+  JSON.tryParse = (json) => Try(() => JSON.parse(json));
+  JSON.tryStringify = (value) => Try(() => JSON.stringify(value));
+}
+
+declare global {
+  interface JSON {
+    tryParse<T>(json: string): Result<T, SyntaxError>;
+    tryStringify<T>(value: T): Result<string, TypeError>;
+  }
+}
---
+++ test/upgrade/JSON.test.ts
@@ -0,0 +1,17 @@
+import '@/upgrade/JSON';
+
+describe('JSON', () => {
+  it('can tryParse', () => {
+    expect(JSON.tryParse('{"foo":"bar"}').unwrap()).toEqual({ foo: 'bar' });
+    expect(JSON.tryParse('{foo: "bar"}').unwrapErr()).toEqual(
+      new SyntaxError("Expected property name or '}' in JSON at position 1")
+    );
+  });
+
+  it('can tryStringify', () => {
+    expect(JSON.tryStringify({ foo: 'bar' }).unwrap()).toEqual('{"foo":"bar"}');
+    expect(JSON.tryStringify({ foo: 15n }).unwrapErr()).toEqual(
+      new TypeError('Do not know how to serialize a BigInt')
+    );
+  });
+});
