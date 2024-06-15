aec8dc0f747c97d0f5fb39ceeb019b97c4d72f1e
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2024-06-15 21:36:44 +0400

    :sparkles: Adds TakeWhilePeek (#16)

---
+++ src/iterators/Peekable/takeWhilePeek.ts
@@ -0,0 +1,8 @@
+import type { PeekableRustIterator } from '../RustIterator.js';
+
+export function* takeWhilePeek<T>(
+  peekable: PeekableRustIterator<T>,
+  predicate: (value: T) => boolean,
+): Generator<T> {
+  while (predicate(peekable.peek().value)) yield peekable.next().value;
+}
